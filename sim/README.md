e75b38075571e21f681b5c3eab4713807d23ac1b
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2024-07-20 10:55:51 -0400

    added simulation data files

--- sim/README.md
+++ sim/README.md
@@ -1,28 +1,30 @@
-# How to run simulations for generating test data
+# Simulating test data for multicam_imu_calib
+
+## How to run simulations for generating test data
 
 
-## Start simulator
+1) Start simulator
 ```
 gz sim -r -s --headless-render ./camera.sdf
 ```
 
-### Start GUI
+2) Start GUI
 ```
 gz sim -g
 ```
 
-### Start bridge
+3) Start bridge
 ```
 ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=./bridge.yaml
 ```
 
-### Start recording
+4) Start recording
 
 ```
 ros2 bag record /camera/image_raw /camera/camera_info /imu /camera/odom
 ```
 
-### Send simulation targets:
+5) Make moves for simulation
 
 ```
 python3 ./control_sim.py
--- sim/control_sim.py
+++ sim/control_sim.py
@@ -8,12 +8,12 @@ def make_cmd(cmd, val):
 
 if __name__ == '__main__':
     cmds = (
+        {'cmd': 'z_rot', 'limits': (0.8, -0.8), 'wait': 2.0},
+        {'cmd': 'y_rot', 'limits': (0.35, -0.35), 'wait': 2.0},
+        {'cmd': 'x_rot', 'limits': (1.570796327, -1.570796327), 'wait': 2.0},
         {'cmd': 'z_trans', 'limits': (0.5, -0.5), 'wait': 2.0},
-#        {'cmd': 'y_trans', 'limits': (-0.5, 0.5), 'wait': 2.0},
-#        {'cmd': 'x_trans', 'limits': (-0.5, 0.5), 'wait': 2.0},
-#        {'cmd': 'z_rot', 'limits': (0.8, -0.8), 'wait': 2.0},
-#        {'cmd': 'y_rot', 'limits': (0.35, -0.35), 'wait': 2.0},
-#        {'cmd': 'x_rot', 'limits': (1.570796327, -1.570796327), 'wait': 2.0},
+        {'cmd': 'y_trans', 'limits': (-0.5, 0.5), 'wait': 2.0},
+        {'cmd': 'x_trans', 'limits': (-0.5, 0.5), 'wait': 2.0},
     )
     for i, c in enumerate(cmds):
         print(f'move: {i}')
